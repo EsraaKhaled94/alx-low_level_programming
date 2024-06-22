9889333c960c4ed440c969c98c8677acb9c0df6c
Esraa Khaled <esraakhaled994@gmail.com>
2024-06-22 04:23:47 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,14 +11,14 @@ int main(void)
 
 	for (number1 = 0; number1 < 99; number1++)
 	{
-		for(number2 = number1 + 1; number2 < 100; number2++)
+		for (number2 = number1 + 1; number2 < 100; number2++)
 		{
 			putchar((number1 / 10) + '0');
 			putchar((number1 % 10) + '0');
 			putchar(' ');
 			putchar((number2 / 10) + '0');
 			putchar((number2 % 10) + '0');
-			if(number1 == 98 && number2 == 99)
+			if (number1 == 98 && number2 == 99)
 			{
 				putchar('\n');
 			}
