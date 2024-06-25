9abd9d635399fac45aaa8bc2d23ad62b6d236485
Esraa Khaled <esraakhaled994@gmail.com>
2024-06-25 12:16:06 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  * print _putchar
  * Return: Always 0 (Success)
  */
-int main (void){
+int main(void){
 	char myString[] = "_putchar";
 	int i;
 
 	for (i = 0; i < (int) strlen(myString); i++)
-       	{
+	{
 		_putchar(myString[i]);
 	}
 	_putchar('\n');
-	return 0;
+	return (0);
 }
