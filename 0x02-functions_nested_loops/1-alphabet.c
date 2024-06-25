30b2bbf7b6d624af7f4022b5ec501b9418fdb442
Esraa Khaled <esraakhaled994@gmail.com>
2024-06-25 12:46:38 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet
- * prints alphabet
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Description: This function uses a loop to iterate through the lowercase
+ * letters of the alphabet from 'a' to 'z' and prints each character using
+ * the _putchar function. After printing all the letters, it prints a newline
+ * character.
  */
 
 void print_alphabet(void)
 {
 	char letter;
+	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
