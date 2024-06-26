eee1f5ef496afa1a41a98ba336a1744181b98a43
Esraa Khaled <esraakhaled994@gmail.com>
2024-06-25 20:28:09 -0700

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ * 
+ * computes the sum of even-valued terms in the Fibonacci sequence
+ * whose values do not exceed 4000000.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	unsigned long fib1 = 1, fib2 = 2, following, sum = 0;
+
+	while(fib1 < 4000000)
+	{
+		if(fib2 % 2 ==0)
+		{
+			sum += fib2;
+		}
+		following = fib1 + fib2;	
+		fib1 = fib2;
+		fib2 = following;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
