9a6abb6dbf6d8ce8dabd907a9c213f2bcfcbee9f
Esraa Khaled <esraakhaled994@gmail.com>
2024-06-25 20:54:49 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,31 @@
  *
  * Return: Always 0 (Success).
  */
-
 int main(void)
 {
-	int i;
-	unsigned long fib1 = 1, fib2 = 2, following;
+	int i = 0;
+	unsigned long n1, n2, quotient1, quotient2, rem1, rem2;
 
-	printf("%lu, %lu", fib1, fib2);
-	for (i = 2; i < 98; i++)
+	n1 = 1, n2 = 2;
+	while (i < 90)
 	{
-		following = fib1 + fib2;
-		printf(", %lu", following);
-		fib1 = fib2;
-		fib2 = following;
+		printf("%lu, %lu, ", n1, n2);
+		n1 += n2, n2 += n1, i += 2;
 	}
-	printf("\n");
+	printf("%lu, %lu, ", n1, n2);
+	quotient1 = n1 / 100, quotient2 = n2 / 100;
+	rem1 = n1 % 100, rem2 = n2 % 100;
+	while (i < 96)
+	{
+		n1 = quotient1 + quotient2, n2 = rem1 + rem2;
+		n2 > 99 ? n1++ : n2;
+		n2 = n2 % 100;
+		printf("%lu", n1);
+		printf(n2 < 10 ? "0" : "");
+		printf("%lu", n2);
+		printf(i < 95 ? ", " : "\n");
+		quotient1 = quotient2, quotient2 = n1;
+		rem1 = rem2, rem2 = n2, i++;
+	}
 	return (0);
 }
