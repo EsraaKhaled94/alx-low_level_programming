cab892c223a5d59e99db596f21f2c708311a8fd6
Esraa Khaled <esraakhaled994@gmail.com>
2024-06-27 12:15:32 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,6 +4,11 @@
  * _isupper - checks if the characteris upper case
  * @c: the character to check
  * Return : 1 if c is uppercase, 0 otherwise
+ *
+ * Description: This function takes an integer input, which is typically the ASCII
+ * value of a character, and determines if it corresponds to an uppercase
+ * letter ('A' through 'Z'). If the character is uppercase, the function
+ * returns 1. Otherwise, it returns 0.
  */
 int _isupper(int c)
 {
