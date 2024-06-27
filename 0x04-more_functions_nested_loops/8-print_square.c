0cb077f2bf72fa26e8e184bace179cb3de319283
Esraa Khaled <esraakhaled994@gmail.com>
2024-06-27 15:34:06 -0700

    8. You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
+/**
+ * print_square - Prints a square followed by a new line.
+ * @size: The size of the square.
+ *
+ * This function prints a square using the character '#'. The size
+ * of the square is determined by the input parameter 'size'. If 'size' is 0 or
+ * less, the function prints only a new line.
+ */
+
 void print_square(int size)
 {
 	int i, j;
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
