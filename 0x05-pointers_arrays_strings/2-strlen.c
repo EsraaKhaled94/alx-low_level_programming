0a2e874e25270d73b34abee8a5e9f54558b13328
Esraa Khaled <esraakhaled994@gmail.com>
2024-07-01 13:09:11 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: Pointer to the string
+ *
+ * Description: This function takes a pointer to a string as its parameter
+ * and returns the length of the string, excluding the null terminator.
+ *
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return length;
+}
