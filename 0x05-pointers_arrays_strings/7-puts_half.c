a7dbe74b1446a2d30d9c4d84f5b46431dc275117
Esraa Khaled <esraakhaled994@gmail.com>
2024-07-01 14:50:35 -0700

    7. Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,9 +5,9 @@
  * @str: Pointer to the string to be printed
  *
  * This function takes a pointer to a string as its parameter
- * and prints the second half of the string, followed by a new line. If the
- * number of characters is odd, it prints the last (length_of_the_string - 1) / 2
- * characters.
+ * and prints the second half of the string, followed by a new line
+ * If the number of characters is odd
+ * it prints the last (length_of_the_string - 1) / 2 characters.
  */
 
 void puts_half(char *str)
