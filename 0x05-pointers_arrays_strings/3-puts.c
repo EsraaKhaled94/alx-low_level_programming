57b0b658245d793ce0555982d1d88bdabe50ff79
Esraa Khaled <esraakhaled994@gmail.com>
2024-07-01 13:19:01 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: Pointer to the string to be printed
+ *
+ * Description: This function takes a pointer to a string as its parameter
+ * and prints the string followed by a new line to the standard output.
+ */
+void _puts(char *str)
+{
+    while (*str != '\0')
+    {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
+}
