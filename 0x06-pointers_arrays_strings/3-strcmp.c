b956db28849ba234386ff176ad744b3caa21e378
Esraa Khaled <esraakhaled994@gmail.com>
2024-07-04 15:59:36 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,16 @@
 /**
+ * _strcmp - Compares two strings
+ * @s1: The first string to be compared
+ * @s2: The second string to be compared
  *
+ * This function compares two strings character by character.
+ * It returns 0 if the strings are equal, a positive value if the first
+ * differing character in s1 is greater than the corresponding character in s2,
+ * and a negative value if the first differing character in s1 is less than
+ * the corresponding character in s2.
  *
+ * Return: An integer less than, equal to, or greater than zero if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
