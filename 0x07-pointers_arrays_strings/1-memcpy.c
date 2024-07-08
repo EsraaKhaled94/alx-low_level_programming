81fba454eb6963d61264df5fe7de7936fcc69d64
Esraa Khaled <esraakhaled994@gmail.com>
2024-07-08 13:55:25 -0700

    1. memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _memcpy - copies memory area
+ * @dest : destination area
+ * @src : source area
+ * @n: size 
+ *
+ * Returns: pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (;i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
