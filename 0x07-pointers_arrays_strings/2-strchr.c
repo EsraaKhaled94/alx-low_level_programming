bcd2a9ec2745b03971e3c5fff0b96412a3004c6f
Esraa Khaled <esraakhaled994@gmail.com>
2024-07-08 16:26:46 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,8 +8,6 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
@@ -17,5 +15,5 @@ char *_strchr(char *s, char c)
 			return (&s[i]);
 		}
 	}
-	return (&s[i]);
+	return ('\0');
 }
