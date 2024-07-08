e317290d2324e87af594a61cb4c75a9c157a827a
Esraa Khaled <esraakhaled994@gmail.com>
2024-07-08 06:11:05 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/**
+ * memset - fills memory with a constant byte.
+ * @s: area to fill
+ * @b: character to fill area
+ * @n: length
+ *
+ * returns area after filling data
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	while (n > 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return (s);
+}
