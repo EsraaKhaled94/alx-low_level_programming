cad438254777811181fa41775cbc4fcd603479bd
Esraa Khaled <esraakhaled994@gmail.com>
2024-07-10 16:58:33 -0700

    3. You mustn't be afraid to dream a little bigger, darling git push

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+/**
+ * factorial - calculates the factorial of a given number
+ * @n: input number
+ *
+ * Return: factorial of number
+ */
+int factorial(int n)
+{
+	int x;
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		x = n * factorial(n - 1);
+	}
+	return (x);
+}
